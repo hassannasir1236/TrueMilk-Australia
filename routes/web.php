0962a982b6dda012e3fc1d5b588702f9201189f2
hassannasir1236 <hassannasir12361236@gmail.com>
2025-04-16 03:02:41 +0500

    project create and configure database and Auth setup properly

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin', function () {
+        return "Welcome Admin!";
+    });
+});
+
+Route::middleware(['auth', 'role:farmer'])->group(function () {
+    Route::get('/farmer', function () {
+        return "Welcome Farmer!";
+    });
+});
+
+Route::middleware(['auth', 'role:customer'])->group(function () {
+    Route::get('/customer', function () {
+        return "Welcome Customer!";
+    });
+});
